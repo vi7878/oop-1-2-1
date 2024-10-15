5703b25342c0640d61edf4443df29a5374c635d4
vi7878 <151910698+vi7878@users.noreply.github.com>
2024-10-15 12:46:38 +0300

    Створив був switch-case для вибору дії над матрицями

--- oop-1-2-1/Program.cs
+++ oop-1-2-1/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Виберіть дії над двома матрицями:");
+            Console.WriteLine("1. Додавання");
+            Console.WriteLine("2. Множення");
+            Console.WriteLine("3. Транспонування");
+            Console.WriteLine("4. Вийти");
+            if (!int.TryParse(Console.ReadLine(), out int choice))
+            {
+                Console.WriteLine("Неправильний ввід. Введіть ще раз.");
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Неправильний вибір. Введіть ще раз.");
+                    break;
+            }
+        }
+    }
+}
+
