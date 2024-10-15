dcf81376b0d6ce6b365c0a30b97734ebbf0477db
vi7878 <151910698+vi7878@users.noreply.github.com>
2024-10-15 13:15:59 +0300

    operator + додавання двох матриць (лише якщо вони мають однаковий розмір); operator * множення двох матриць (лише якщо кількість стовпчиків першої дорівнює кількості рядків другої)

--- oop-1-2-1/MatrixOperations.cs
+++ oop-1-2-1/MatrixOperations.cs
@@ -14,4 +14,17 @@ public partial class MyMatrix
 
         return new MyMatrix(result);
     } 
+    public static MyMatrix operator *(MyMatrix a, MyMatrix b)
+    {
+        if (a.Width != b.Height)
+            throw new ArgumentException("К-ть рядків першої матриці має співпадати з к-тю стовпчиків іншої");
+
+        double[,] result = new double[a.Height, b.Width];
+        for (int i = 0; i < a.Height; i++)
+        for (int j = 0; j < b.Width; j++)
+        for (int k = 0; k < a.Width; k++)
+            result[i, j] += a[i, k] * b[k, j];
+
+        return new MyMatrix(result);
+    }
 }
