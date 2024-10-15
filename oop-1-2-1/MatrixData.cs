d34fc3c78e1a0c13888a30719fb8e062a30303a6
vi7878 <151910698+vi7878@users.noreply.github.com>
2024-10-15 12:57:44 +0300

    Контструктори з зубчастого масиву

--- oop-1-2-1/MatrixData.cs
+++ oop-1-2-1/MatrixData.cs
@@ -12,4 +12,17 @@ public partial class MyMatrix
         data = new double[Height, Width];
         Array.Copy(other.data, data, other.data.Length);
     }
+    public MyMatrix(double[][] jaggedArray)
+    {
+        if (!jaggedArray.All(row => row.Length == jaggedArray[0].Length))
+            throw new ArgumentException("Зубчастий масив - це не прямокутний");
+
+        Height = jaggedArray.Length;
+        Width = jaggedArray[0].Length;
+        data = new double[Height, Width];
+
+        for (int i = 0; i < Height; i++)
+        for (int j = 0; j < Width; j++)
+            data[i, j] = jaggedArray[i][j];
+    }
 }
