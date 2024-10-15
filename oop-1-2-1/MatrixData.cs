41ec7d125c3767171b07d9d566c7fe5f93106ab6
vi7878 <151910698+vi7878@users.noreply.github.com>
2024-10-15 13:01:06 +0300

    Прямокутна матриця

--- oop-1-2-1/MatrixData.cs
+++ oop-1-2-1/MatrixData.cs
@@ -25,4 +25,21 @@ public partial class MyMatrix
         for (int j = 0; j < Width; j++)
             data[i, j] = jaggedArray[i][j];
     }
+    public MyMatrix(string[] rows)
+    {
+        var parsedRows = rows.Select(row => 
+            row.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray()
+        ).ToArray();
+        if (!parsedRows.All(row => row.Length == parsedRows[0].Length))
+            throw new ArgumentException("Масив рядків не представляє прямокутну матрицю");
+        Height = parsedRows.Length;
+        Width = parsedRows[0].Length;
+        data = new double[Height, Width];
+        for (int i = 0; i < Height; i++)
+        for (int j = 0; j < Width; j++)
+            data[i, j] = parsedRows[i][j];
+    }
+    
 }
